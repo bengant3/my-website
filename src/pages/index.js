d0e5d8a05ea13f125da74467fe3f4c71225ce537
Ben Gant <bengant3@gmail.com>
2022-07-05 20:18:02 -0400

    moved favicon script to head

--- src/pages/index.js
+++ src/pages/index.js
@@ -23,7 +23,6 @@ const IndexPage = () => {
   return (
     <main style={styles.page}>
       <title>Ben Gant</title>
-      <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🐯</text></svg>"></link>
       <h1 style={styles.mainHeader}>
         🐯 Ben Gant
       </h1>
